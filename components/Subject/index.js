b12a1e182eac7ae057d81ada3e07e1e1390c7143
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-01-09 15:05:06 -0300

    Adiciona Subject para desktop/app

--- components/Subject/index.js
+++ components/Subject/index.js
@@ -1,43 +1,60 @@
-import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled, {withTheme} from 'styled-components';
+import mobileStyled from 'styled-components/native';
 
-import colors from '../../styles/variables/colors.json';
-import {SubjectTypography} from '../Typography';
+const getContainerStyle = props => `
+  height: 28px;
+  border-radius: 5px;
+  background-color: ${props.subjectColor ? props.subjectColor : props.theme.colors.primary1};
+  width: max-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${props.theme.parseMargin(props.theme, props)};
+  ${props.theme.parsePadding(props.theme, props)};
+`;
+const getSubjectTypographyStyle = props => `
+  font-family: ${props.theme.fonts.secondary};
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 22px;
+  color: white;
+  text-transform: uppercase;
+`;
+const Container = styled.div`${props => getContainerStyle(props)}`;
+const SubjectTypography = styled.span`${props => getSubjectTypographyStyle(props)}`;
+const MobileContainer = mobileStyled.View`${props => getContainerStyle(props)}`;
+const MobileSubjectTypography = mobileStyled.Text`${props => getSubjectTypographyStyle(props)}`;
 
 const Subject = props => {
-  const {children, color, className, style, size, filled, weight} = props;
-
-  if (!children) return null;
-
-  const classes = classnames({
-    'Prensa-Subject-root': true,
-    'filled': filled,
-    [`color-${color}`]: true,
-    [`${className}`]: className
-  });
-
+  const {title, theme, mb, px, isMobile} = props;
+  if (!title) return null;
+  if (isMobile) {
+    return (
+      <MobileContainer mb={mb} px={px} subjectColor={theme.activeColor}>
+        <MobileSubjectTypography>{title}</MobileSubjectTypography>
+      </MobileContainer>
+    );
+  }
   return (
-    <div className={classes} style={style}>
-      <SubjectTypography size={size} color={filled ? 'white' : color} weight={weight}>{children}</SubjectTypography>
-    </div>
+    <Container mb={mb} px={px} subjectColor={theme.activeColor}>
+      <SubjectTypography>{title}</SubjectTypography>
+    </Container>
   );
 };
 
 Subject.defaultProps = {
-  color: 'primary-1',
-  filled: false,
-  size: 1
+  mb: 3,
+  px: 1
 };
 
 Subject.propTypes = {
-  children: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  color: PropTypes.oneOf(colors),
-  filled: PropTypes.bool,
-  style: PropTypes.object,
-  size: PropTypes.oneOf([1, 2, 3]),
-  weight: PropTypes.oneOf(['thin', 'light', 'regular', 'medium', 'bold', 'italic'])
+  isMobile: PropTypes.bool,
+  mb: PropTypes.number,
+  px: PropTypes.number,
+  title: PropTypes.string,
+  theme: PropTypes.object,
 };
 
-export default Subject;
+export default withTheme(Subject);
